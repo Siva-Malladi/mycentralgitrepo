59bfc26e68bea4c9ca0d0bfdf27705e904ad8328
sivamalladi <sivamalladi999@gmail.com>
2021-11-05 13:47:55 +0530

    commit-1

---
+++ otpgenerate.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+import math
+import random
+
+
+def generateotp(size_of_otp):
+	alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	otp = ""
+	alpnum_size = len(alphanumeric)
+
+	for i in range(size_of_otp):
+		otp += alphanumeric[math.floor(random.random() * alpnum_size)] 
+
+	print(otp)
+
+
+if __name__ == "__main__":
+	size_of_otp = int(input())
+	generateotp(size_of_otp)
